28cb2b8efe4c3682af9fa17b890b9dc7b7efa0fd
liteit <cloud@liteit.se>
2024-09-08 20:07:11 +0200

    test

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -24,7 +24,7 @@ const Navigation = () => {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-xl font-bold">AirAccounter</Link>
+        <Link to="/" className="text-xl font-bold">AirAccounter2</Link>
         <div className="space-x-4">
           <Link to="/dashboard" className="hover:text-gray-300">Dashboard</Link>
           <Link to="/transactions" className="hover:text-gray-300">Transactions</Link>
